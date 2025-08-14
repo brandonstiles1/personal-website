9156b4262669540420bceabc60b0c20824b8d1c2
Brandon Personal <brandonstiles@gmail.com>
2025-08-14 17:12:47 -0400

    Adds Github Pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import github from "@astrojs/github";
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    integrations: [github()],
+    site: 'https://brandonstiles1.github.io/personal-website/'
+});
